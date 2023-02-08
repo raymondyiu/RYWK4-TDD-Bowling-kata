9ff4c7ddc1ade99740fa4090d39f8e5ee17b1bb0
Raymond <raymond.yiu@gmail.com>
2023-02-08 12:56:50 +0000

    Test pass for all frames with two digits

--- src/main/java/org/example/Bowling.java
+++ src/main/java/org/example/Bowling.java
@@ -1,6 +1,53 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Bowling {
+
+    private static final String COMMA_DELIMITER = ",";
+    private List<List<String>> csvData;
+    private int convert2value(char inputChar){
+        // if 'X', frameScore[0][i]=10
+        // if '/', frameScore[0][i]=10
+        // if '-', frameScore[0][i]=0
+        // if [1..9] frameScore[0][i]=no of pins
+        // other return -1
+        // use switch statement
+        switch (inputChar) {
+            case '-':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            case 'X':
+                return 10;
+            case '/':
+                return 10;
+            default:
+                return 0;
+        }
+    }
     public int score (String frameStr){
         // create spare Flag store the state
         boolean spareFlag=false;
@@ -23,15 +70,15 @@ public class Bowling {
         for (int i=0; i<frames.length; i++){
             // use String.charAt() to get character
             char firstChar = frames[i].charAt(0);
-            // if 'X', frameScore[0][i]=10
-            // if '/', frameScore[0][i]=10
-            // if '-', frameScore[0][i]=0
-            // if [1..9] frameScore[0][i]=no of pins
-            // use switch method
-            switch(firstChar){
-                case '1':
-                    frameScore[0][i] = 1;
-                    break;
+
+            // if 'X', check first character only
+            if (firstChar == 'X') {
+                frameScore[0][i] = 10;
+            }  else {
+
+                frameScore[0][i] = convert2value(firstChar);
+                char secondChar = frames[i].charAt(1);
+                frameScore[0][i] += convert2value(secondChar);
             }
 
             // if strikeMode
@@ -43,4 +90,5 @@ public class Bowling {
 
         return totalScore;
     }
+
 }
--- src/test/java/org/example/BowlingTest.java
+++ src/test/java/org/example/BowlingTest.java
@@ -1,14 +1,50 @@
 package org.example;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import org.junit.jupiter.api.Test;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingTest {
 
+    public List<String[]> readLineByLine(String filename) {
+        List<String[]> allData = new ArrayList<>();
+        try {
+        Path path = Paths.get(
+                ClassLoader.getSystemResource(filename).toURI());
+            Reader reader = Files.newBufferedReader(path);
+            CSVReader csvReader = new CSVReaderBuilder(reader).build();
+                allData = csvReader.readAll();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return allData;
+    }
+
     @Test
     void score() {
+        List<String[]> csvTestCases = new ArrayList<>();;
+        csvTestCases = readLineByLine("testcases.csv");
+
         Bowling bowling = new Bowling();
-        assertEquals(10, bowling.score("1- 1- 1- 1- 1- 1- 1- 1- 1- 1-"));
+
+        for (String[] item : csvTestCases) {
+            System.out.println(item[0] + "\t" + item[1]);
+            assertEquals(Integer.valueOf(item[0]), bowling.score(item[1]));
+        }
+
+
+
+
+
     }
 }
